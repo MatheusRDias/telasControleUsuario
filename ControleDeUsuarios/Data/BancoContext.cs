905cec140d2e10943e1325c2dcd85f4e2f654d40
matheus.dias <matheus.dias@GPK597>
2022-08-10 08:43:48 -0300

    Add project files.

---
+++ ControleDeUsuarios/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeUsuarios.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext (DbContextOptions<BancoContext>options) : base(options)
+        {
+
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
---
+++ ControleDeUsuarios/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeUsuarios.Models
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int email { get; set; }
+        public int celular { get; set; }
+
+    }
+}
